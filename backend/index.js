9056c71493db948d09c6507b0f7248a4affe6fce
meow219 <m.prabhu8428@gmail.com>
2024-01-21 20:40:58 +0530

    Chumma

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
